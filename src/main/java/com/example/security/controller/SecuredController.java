71529b81c2fbe4c7b7b9acc8742036ea94fa3c92
Adam Mazurkiewicz <adam.mazurkiewicz@cg24.com>
2025-12-18 21:16:03 +0100

    custom security filter added, default form base auth deleted as its in ServletWebSecurityAutoConfiguration.class

--- src/main/java/com/example/security/controller/SecuredController.java
+++ src/main/java/com/example/security/controller/SecuredController.java
@@ -1,4 +1,4 @@
-package com.example.security;
+package com.example.security.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
