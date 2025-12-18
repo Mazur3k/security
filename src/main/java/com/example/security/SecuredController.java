d4641950ec16ccade00970b779b04ed934626433
Adam Mazurkiewicz <adam.mazurkiewicz@cg24.com>
2025-12-18 20:41:53 +0100

    secured first controller added

---
+++ src/main/java/com/example/security/SecuredController.java
@@ -0,0 +1,15 @@
+package com.example.security;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class SecuredController {
+    @GetMapping("hello")
+    public ResponseEntity<String> securedGet(){
+        return ResponseEntity.ok("Hello Spring Security!");
+    }
+}
